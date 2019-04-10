72fb24e94d9637527470a3caef08388d56171f38
-001 <1219288694@qq.com>
2019-04-10 19:52:54 +0800

    增加用户模块 加入Spring Websocket

---
+++ js/im_index/spring-test.js
@@ -0,0 +1,61 @@
+
+
+
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+// 连接
+function connect() {
+    var url = "http://127.0.0.1:8080/gs-guide-websocket"
+    var socket = new SockJS(url);
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            console.log("3333 " + greeting);
+            // 返回来的内容 greetin
+            // 用JSON.parse进行解析
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+// 关闭连接
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+// 发送名字
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+// 显示回来的内容
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
